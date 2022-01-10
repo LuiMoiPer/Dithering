8349ee760536a63ec83dbedde38d72658334f388
LuiMoiPer <luimoiper@outlook.com>
2022-01-10 15:26:16 -0800

    Class to hold premade pallets

---
+++ src/Pallet.java
@@ -0,0 +1,87 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.awt.Color;
+
+public class Pallet {
+    static Set<Color> KiroKaze() {
+        Set<Color> pallet = new HashSet<>();
+        pallet.add(new Color(51, 44, 80));
+        pallet.add(new Color(70, 135, 143));
+        pallet.add(new Color(148, 227, 68));
+        pallet.add(new Color(226, 243, 228));
+        return pallet;
+    }
+
+    static Set<Color> Twilight5() {
+        Set<Color> pallet = new HashSet<>();
+        pallet.add(new Color(251, 187, 173));
+        pallet.add(new Color(238, 134, 149));
+        pallet.add(new Color(74, 122, 150));
+        pallet.add(new Color(51, 63, 88));
+        pallet.add(new Color(41, 40, 49));
+        return pallet;
+    }
+
+    static Set<Color> Pollen8() {
+        Set<Color> pallet = new HashSet<>();
+        pallet.add(new Color(115, 70, 76));
+        pallet.add(new Color(171, 86, 117));
+        pallet.add(new Color(238, 106, 124));
+        pallet.add(new Color(255, 167, 165));
+        pallet.add(new Color(255, 224, 126));
+        pallet.add(new Color(255, 231, 214));
+        pallet.add(new Color(114, 220, 187));
+        pallet.add(new Color(52, 172, 186));
+        return pallet;
+    }
+
+    static Set<Color> FUNKYFUTURE8() {
+        Set<Color> pallet = new HashSet<>();
+        pallet.add(new Color(43, 15, 84));
+        pallet.add(new Color(171, 31, 101));
+        pallet.add(new Color(255, 79, 105));
+        pallet.add(new Color(255, 247, 248));
+        pallet.add(new Color(255, 129, 66));
+        pallet.add(new Color(255, 218, 69));
+        pallet.add(new Color(51, 104, 220));
+        pallet.add(new Color(73, 231, 236));
+        return pallet;
+    }
+
+    static Set<Color> FUZZYFOUR() {
+        Set<Color> pallet = new HashSet<>();
+        pallet.add(new Color(48, 35, 135));
+        pallet.add(new Color(255, 55, 150));
+        pallet.add(new Color(0, 250, 172));
+        pallet.add(new Color(255, 253, 175));
+        return pallet;
+    }
+
+    static Set<Color> CAPP5() {
+        Set<Color> pallet = new HashSet<>();
+        pallet.add(new Color(250, 221, 162));
+        pallet.add(new Color(107, 97, 255));
+        pallet.add(new Color(102, 161, 255));
+        pallet.add(new Color(142, 205, 230));
+        pallet.add(new Color(240, 239, 244));
+        return pallet;
+    }
+
+    static Set<Color> CGA1HIGH() {
+        Set<Color> pallet = new HashSet<>();
+        pallet.add(new Color(0, 0, 0));
+        pallet.add(new Color(255, 85, 255));
+        pallet.add(new Color(85, 255, 255));
+        pallet.add(new Color(255, 255, 255));
+        return pallet;
+    }
+
+    static Set<Color> SODACAP() {
+        Set<Color> pallet = new HashSet<>();
+        pallet.add(new Color(33, 118, 204));
+        pallet.add(new Color(255, 125, 110));
+        pallet.add(new Color(252, 166, 172));
+        pallet.add(new Color(232, 231, 203));
+        return pallet;
+    }
+}
